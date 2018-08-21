fd6fe8f9c404c36b3c2d9c4c130dfea4fc7cd92d
FredrickI <fredrick.ifebuzo@gmail.com>
2018-08-20 20:50:55 -0400

    Add files via upload

---
+++ lists/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render , redirect
+from lists.forms import ItemForm
+from lists.models import List
+
+
+
+
+def home_page(request):
+    
+    return render(request, 'home.html', {'form': ItemForm())
+    
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    form = ItemForm()
+    if request.method == 'POST':
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            form.save(for_list=list_)
+            return redirect(list_)
+    return render(request, 'list.html', {'list': list_, "form": form})
+
+     
+def new_list(request):
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        form.save(for_list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {"form" : form})
+
+def add_item(request):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
+
